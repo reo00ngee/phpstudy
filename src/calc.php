a86a98d66910590e1850b619e814973cf6f9f138
reo00ngee <reo00ngee@gmail.com>
2023-03-13 23:45:52 +0900

    first

---
+++ src/calc.php
@@ -0,0 +1,29 @@
+<?php
+// 以下の式の値を計算してください。
+// 1×2÷3+4×5÷6+7×8÷9+⋯+10099×10100÷10101
+
+$num = 1;
+$num1 = [];
+$num2 = [];
+$num3 = [];
+$i = 0;
+$sum = 0;
+$sum2 = 0;
+
+while ($num <= 10101) {
+  if ($num % 3 == 1) {
+    $num1[] = $num;
+  } elseif ($num % 3 == 2) {
+    $num2[] = $num;
+  } elseif ($num % 3 == 0) {
+    $num3[] = $num;
+  }
+  $num++;
+}
+
+while ($i <= 3366) {
+  $sum = $num1[$i] * $num2[$i] / $num3[$i];
+  $sum2 = $sum2 + $sum;
+  $i++;
+}
+echo $sum2 .PHP_EOL;
